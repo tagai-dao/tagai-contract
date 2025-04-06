9b507977903d1499f2518d13c2e95bad9a6b0b30
terry3t <wangxi8206@sina.com>
2025-04-06 21:21:18 +0800

    update custom errors

--- contracts/CoinPurse.sol
+++ contracts/CoinPurse.sol
@@ -7,12 +7,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-
+import "./interface/ICoinPurse.sol";
 interface IWBNB {
     function withdraw(uint wad) external;
 }
 
-contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
+contract CoinPurse is Ownable, Pausable, ReentrancyGuard, ICoinPurse {
     event Tip(address indexed from, address indexed to, address indexed token, uint toXId, uint amount);
 
     event LimitSet(address indexed user, address indexed token, uint256 maxPerTx, uint256 maxPerDay);
@@ -42,7 +42,7 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
     mapping(uint => address) public alreadyWithdraw;
 
     uint public feeRate = 500;
-    uint public minFee = 0.001 ether;
+    uint public minFee = 0.0005 ether;
     uint denominator = 10000;
 
     constructor() Ownable(msg.sender) {
@@ -74,7 +74,7 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
 
     function _checkAndUpdateLimit(address user, address token, uint256 amount) internal {
         Limit storage limit = userLimits[user][token];
-        require(amount <= limit.maxPerTx, "Exceeds per-tx limit");
+        if (amount > limit.maxPerTx) revert ExceedsPerLimit();
 
         uint256 today = _getToday();
         if (limit.lastUpdatedDay < today) {
@@ -82,7 +82,7 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
             limit.lastUpdatedDay = today;
         }
 
-        require(limit.spentToday + amount <= limit.maxPerDay, "Exceeds daily limit");
+        if (limit.spentToday + amount > limit.maxPerDay) revert ExceedsDailyLimit();
         limit.spentToday += amount;
     }
 
@@ -96,7 +96,7 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
 
     function withdraw(uint xId, address[] calldata tokens, bytes calldata signature) external whenNotPaused nonReentrant {
         bytes32 data = keccak256(abi.encodePacked(block.chainid, xId, tokens, msg.sender));
-        require(_check(data, signature), "Invalid signature");
+        if (!_check(data, signature)) revert InvalidSignature();
 
         uint[] memory amounts;
         for (uint i = 0; i < tokens.length; i++) {
@@ -111,11 +111,11 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
     function tip(address user, address token, address to, uint256 toXId, uint256 amount) external onlyOperator nonReentrant whenNotPaused {
         _checkAndUpdateLimit(user, token, amount);
         if (to != address(0)) {
-            require(IERC20(token).transferFrom(user, to, amount), "Transfer failed");
+            if (!IERC20(token).transferFrom(user, to, amount)) revert TransferFailed();
         } else {
-            require(toXId != uint(0), "Invalid toXId");
-            require(alreadyWithdraw[toXId] == address(0), "Already withdraw");
-            require(IERC20(token).transferFrom(user, address(this), amount), "Transfer failed");
+            if (toXId == uint(0)) revert InvalidToXId();
+            if (alreadyWithdraw[toXId] != address(0)) revert AlreadyWithdraw();
+            if (!IERC20(token).transferFrom(user, address(this), amount)) revert TransferFromFailed();
             hostingAmount[toXId][token] += amount;
         }
 
@@ -132,7 +132,7 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
         _checkAndUpdateLimit(user, WBNB, amountIn);
 
         // Step 1: Transfer from user
-        require(IERC20(WBNB).transferFrom(user, address(this), amountIn), "TransferFrom failed");
+        if (!IERC20(WBNB).transferFrom(user, address(this), amountIn)) revert TransferFromFailed();
 
         // Step 2: Extract to bnb
         IWBNB(WBNB).withdraw(amountIn);
@@ -141,7 +141,7 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
         (bool success, bytes memory receiveAmount) = tokenOut.call{value: amountIn}(
             abi.encodeWithSignature("buyToken(uint256,address,uint16)", 0, sellsman, slippage)
         );
-        require(success, "buyToken failed");
+        if (!success) revert BuyTokenFailed();
 
         // Step 4: send to user
         IERC20(tokenOut).transfer(user, abi.decode(receiveAmount, (uint256)));
@@ -154,18 +154,18 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
         address[] calldata path,
         uint256 deadline
     ) external onlyOperator nonReentrant whenNotPaused {
-        require(path.length >= 2, "Invalid path");
+        if (path.length < 2) revert InvalidPath();
         address tokenIn = path[0];
-        require(tokenIn == WBNB, "Invalid path[0]");
+        if (tokenIn != WBNB) revert InvalidPath();
         _checkAndUpdateLimit(user, tokenIn, amountIn);
 
         // Step 1: Transfer from user
-        require(IERC20(tokenIn).transferFrom(user, address(this), amountIn), "TransferFrom failed");
+        if (!IERC20(tokenIn).transferFrom(user, address(this), amountIn)) revert TransferFromFailed();
 
         uint fee = _getFee(amountIn);
         IWBNB(WBNB).withdraw(fee);
         (bool success, ) = feeAddress.call{value: fee}("");
-        require(success, "Fee failed");
+        if (!success) revert CostFeeFailed();
 
         // Step 2: Approve to router
         require(IERC20(tokenIn).approve(address(router), amountIn - fee), "Approve failed");
@@ -186,12 +186,12 @@ contract CoinPurse is Ownable, Pausable, ReentrancyGuard {
     }
 
     function setFeeAddress(address _feeAddress) external onlyOwner {
-        require(_feeAddress != address(0), "Invalid address");
+        if (_feeAddress == address(0)) revert InvalidAddress();
         feeAddress = _feeAddress;
     }
 
     function setOperator(address _operator) external onlyOwner {
-        require(_operator != address(0), "Invalid address");
+        if (_operator == address(0)) revert InvalidAddress();
         operator = _operator;
     }
 
---
+++ contracts/interface/ICoinPurse.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+interface ICoinPurse {
+    error ExceedsPerLimit();
+    error ExceedsDailyLimit();
+    error InvalidSignature();
+    error InvalidToXId();
+    error AlreadyWithdraw();
+    error TransferFailed();
+    error TransferFromFailed();
+    error BuyTokenFailed();
+    error InvalidPath();
+    error CostFeeFailed();
+    error InvalidAddress();
+    
+    
+    
+}
