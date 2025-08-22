f2673273965a7dc1ad4acbb45e4b15c5b2431b21
terry <terry3t@gmail.com>
2025-08-22 17:52:23 +0800

    update bonding curve to x-layer chain

--- bonding_curve.py
+++ bonding_curve.py
@@ -4,34 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-# 定义变量
-a, x = symbols('a x', real=True)
 
-# 定义方程 y = a - b / (x + 5 * 10^26)
-y = 6_500_000_000 * exp(x / a)
+print(6.5 / math.log(15))
 
-# 条件 1：x = 0 时，y = 1.5e9
-# condition1 = Eq(y.subs(x, 0), 1_400_000_000)
-
-# 条件 2：x = 7e26 时，y = 2e10
-condition2 = Eq(y.subs(x, 6.5 * 10**26), 20_000_000_000)
-
-# 解方程组
-solutions = solve([condition2], (a))
-
-print(solutions)
-# a = solutions[1]
-# b = solutions[1]
-
-b = 2.5175516438e26
-
-# #  1400000000    0.0000000014
-# #  20 000 000 000    0.000000020
-# {a: 9/280000000000000000, b: -12000000000/7}
-
-# Define the function y = 177000000000/7 - 33300000000000000000000000000000000000/7/(x + 2 * 10^26)y = 177000000000/7 - 33300000000000000000000000000000000000/7/(x + 2 * 10^26)
 def y_function(x):
-    return 6_500_000_000 * math.exp(x / 2.5175516438e26)
+    return 6e9 * math.exp(x / 2.400250924947558e26)
 
 integral_result, error = quad(y_function, 0, 10**24)
 
@@ -65,9 +42,9 @@ integral_result, error = quad(y_function, 0, 6.5 * 10**26)
 
 print(f"6.5亿代币需要: {integral_result / 10**36}")
 
-print(f"初始价格为：{y_function(0) / 10 ** 18} USDT, 市值为：{y_function(0) / 10 ** 9}")
-print(f"bc结束价格为：{y_function(6.5 * 10**26) / 10 ** 18} USDT, 市值为：{y_function(6.5 * 10**26) / 10 ** 9}")
-print(f"Dex 价格为：{20 / 200000000}")
+print(f"初始价格为：{y_function(0) / 10 ** 18} OKB, 市值为：{y_function(0) / 10 ** 9}")
+print(f"bc结束价格为：{y_function(6.5 * 10**26) / 10 ** 18} OKB, 市值为：{y_function(6.5 * 10**26) / 10 ** 9}")
+print(f"Dex 价格为：{19.162 / 200000000}")
 
 # Generate x values from 0 to 7e26
 x_values = np.linspace(0, 6.5 * 10**26, 500)
@@ -80,7 +57,7 @@ for a in x_values:
 
 # Plot the function
 plt.figure(figsize=(12, 6))
-plt.plot(x_values, y_values, label=r'$y = 1.4 \times 10^9 \times e^{\frac{x}{2.4442889787856833 \times 10^{26}}}$', color='b')
+plt.plot(x_values, y_values, label=r'$P = 6 \times 10^9 \times e^{\frac{x}{2.400250924947558 \times 10^{26}}}$', color='b')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.grid(True)
