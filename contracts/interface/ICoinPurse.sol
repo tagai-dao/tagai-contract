a8dac39527b499956835970edd82ea4debcf2e58
terry <terry3t@gmail.com>
2025-09-01 14:03:03 +0800

    add interface

---
+++ contracts/interface/ICoinPurse.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+interface ICoinPurse {
+    event Tip(address indexed from, address indexed to, address indexed token, uint toXId, uint amount);
+
+    event LimitSet(address indexed user, address indexed token, uint256 maxPerTx, uint256 maxPerDay);
+
+    event Withdraw(uint indexed xId, address indexed user, address[] tokens, uint[] amounts);
+}
