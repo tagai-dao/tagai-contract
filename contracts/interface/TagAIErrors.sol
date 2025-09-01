5bd850ac8336cb21358a23b02ca81d85cd3c56e5
terry <terry3t@gmail.com>
2025-09-01 14:04:04 +0800

    add interface

---
+++ contracts/interface/TagAIErrors.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+interface TagAIErrors {
+    error ExceedsPerLimit();
+    error ExceedsDailyLimit();
+    error InvalidSignature();
+    error InvalidToXId();
+    error AlreadyWithdraw();
+    error TransferFailed();
+    error TransferFromFailed();
+    error BuyTokenFailed();
+    error InvalidPath();
+    error CostFeeFailed();
+    error InvalidAddress();
+    error InsufficientFee();
+    error MulticallFailed();
+    error OrderIdUsed();
+    error ERC20InsufficientAllowance(address spender, uint256 currentAllowance, uint256 value);
+    error ERC20InsufficientBalance(address from, uint256 fromBalance, uint256 value);
+    error InsufficientBalance();
+    error InsufficientAllowance();
+}
