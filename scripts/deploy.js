3bdeb0875c2975a02c30cb6f5787b9864eeabe0c
terry3t <wangxi8206@sina.com>
2025-09-08 18:57:41 +0800

    deploy new pump

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,18 +4,18 @@ async function main() {
     console.log('start')
     const [signer] = await ethers.getSigners();
     console.log("deployer:", signer.address, 'balance:', await signer.provider.getBalance(signer.address), '\n', await signer.provider.getFeeData())
+// return;
+    // const coinPurse = await ethers.deployContract('CoinPurse');
+    // console.log(1, coinPurse.target)
+    // return;
+    // const ipshare = await ethers.deployContract('IPShare');
+    // console.log(1, ipshare.target)
 
-    const coinPurse = await ethers.deployContract('CoinPurse');
-    console.log(1, coinPurse.target)
-    return;
-    const ipshare = await ethers.deployContract('IPShare');
-    console.log(1, ipshare.target)
-
-    const pump = await ethers.deployContract('Pump', [ipshare.target])
+    const pump = await ethers.deployContract('Pump', ['0x7B0ddC305C32AAEbabc0FE372a4460e9903e95D0'])
     console.log(2, pump.target)
 
-    const wrappUni = await ethers.deployContract('WrappedUniV2ForTagAI');
-    console.log(3, wrappUni.target);
+    // const wrappUni = await ethers.deployContract('WrappedUniV2ForTagAI');
+    // console.log(3, wrappUni.target);
 }
 
 main().catch(error => {
